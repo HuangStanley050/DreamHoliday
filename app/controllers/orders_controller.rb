faabe81f888e7c9664523a1769bfd596d8432d95
Stanley Huang <huang.stanley050@gmail.com>
2019-10-25 14:08:07 +1100

    Orders and Holiday controller and views

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,16 @@
+class OrdersController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def success
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'orders/index'
+  get 'orders/create'
+  get 'orders/edit'
+  get 'orders/success'
+  get 'orders/show'
+  get 'holidays/index'
+  get 'holidays/edit'
+  get 'holidays/create'
+  get 'holidays/show'
+  get 'holidays/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
