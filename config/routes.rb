6416f57d54515d548a0ae9d74c2a3a4aa7f6e8f5
Stanley Huang <huang.stanley050@gmail.com>
2019-10-25 14:34:52 +1100

    doing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,22 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'orders/index'
   get 'orders/create'
   get 'orders/edit'
   get 'orders/success'
   get 'orders/show'
+  post 'orders/create', to: 'orders#create'
+  patch 'orders/edit/:id', to: 'orders#edit'
+  delete 'orders/delete/:id', to: 'orders#destroy'
+
   get 'holidays/index'
   get 'holidays/edit'
   get 'holidays/create'
   get 'holidays/show'
-  get 'holidays/destroy'
+  patch 'holidays/edit/:id', to: 'holidays#edit'
+  delete 'holidays/destroy/:id', to: 'holidays#destroy'
+  post 'holidays/create', to: 'holidays#create'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
