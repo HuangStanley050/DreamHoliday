2f75a8c387971e3924d947cd6350e634b2ec1695
Stanley Huang <huang.stanley050@gmail.com>
2019-10-28 15:17:14 +1100

    not able to save to Holiday table, some reference issue

--- app/controllers/holidays_controller.rb
+++ app/controllers/holidays_controller.rb
@@ -1,18 +1,36 @@
 # frozen_string_literal: true
 
 class HolidaysController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: %i[edit new show]
 
-  def index; end
+  def index
+    # @listings = current_user.holidays
+  end
 
   def edit; end
 
   def new
-    @holiday = Holiday.new
+    if current_user.vendor
+      @holiday = Holiday.new
+    else
+      redirect_to '/holidays/index'
+    end
   end
 
   def create
-    p params
+    # p current_user
+    # p params
+    # p holiday_params
+    p "Current user id #{current_user.id}"
+    @vendor = User.find(current_user.id)
+
+    @new_holiday = @vendor.holiday.create(holiday_params)
+    if @new_holiday.save
+      redirect_to '/holidays/show'
+    else
+      p @new_holiday.errors
+      puts 'something wrong happened'
+    end
   end
 
   def show; end
@@ -21,7 +39,7 @@ class HolidaysController < ApplicationController
 
   private
 
-  def create_holiday_params
-    params.require(:holiday).permit(:title, :description, :picturUrl, :price, :user)
+  def holiday_params
+    params.require(:holiday).permit(:title, :description, :pictureUrl, :price)
   end
 end
