22018b5fa5f2372bd252b840476ce636c628db96
Stanley Huang <huang.stanley050@gmail.com>
2019-10-30 14:00:03 +1100

    able to save the order to databse but the price is wrong

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'date'
+
 class OrdersController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:webhook]
   before_action :authenticate_user!, only: %i[edit new show]
@@ -16,7 +18,7 @@ class OrdersController < ApplicationController
 
   def show
     @holiday = Holiday.find(params[:id])
-    price = Integer(@holiday.price)
+    price = @holiday.price.to_i * 100
     session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
       customer_email: current_user.email,
@@ -48,16 +50,28 @@ class OrdersController < ApplicationController
     user_id = payment.metadata.user_id
     totalPrice = payment.metadata.amount
 
-    date = 0
+    date = Date.current
 
-    # orderParam = {"totalPrice":totalPrice, "date": date}
+    orderParam = { "priceTotal": totalPrice, "date": date, "user_id": user_id, "holiday_id": listing_id }
 
-    # @newOrder_user = current_user.orders.new()
-    # @newOrder_holiday = Holiday.find(listing_id).orders.new()
-    p 'total price ' + totalPrice
-    p 'holiday id ' + listing_id
-    p 'user id ' + user_id
+    # @newOrder_user = User.find(user_id).orders.new(orderParam)
+    # @newOrder_holiday = Holiday.find(listing_id).orders.new(orderParam)
 
-    status 200
+    # <Order id: nil, priceTotal: 100000, date: "2019-10-30", created_at: nil, updated_at: nil, holiday_id: nil, user_id: 15>
+
+    @newOrder = Order.new(orderParam)
+
+    if @newOrder.save
+      puts 'Saved to database'
+      status 200
+    else
+      puts 'something went wrong'
+      status 200
+    end
+
+    # p 'total price ' + totalPrice
+    # p 'holiday id ' + listing_id
+    # p 'user id ' + user_id
+    # p 'purchase time ' + date.to_s
   end
 end
--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -2,5 +2,6 @@
 
 class Holiday < ApplicationRecord
   belongs_to :user
+  has_many :orders
   has_one_attached :pictureUrl
 end
